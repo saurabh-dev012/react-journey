09ff5ad4a9198ec4a484bb499aba31bd5b6fb289
saurabh-dev012 <sp4250838@gmail.com>
2026-08-30 16:03:24 +0530

    Update project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>
+        Add value: {counter}
+      </button>
+
+      <br />
+
+      <button onClick={removeValue}>
+        Remove value: {counter}
+      </button>
+    </>
+  )
+}
+
+export default App
