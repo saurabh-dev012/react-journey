f3c5929b506862a28900563f224008a45bd347b2
Rohan Sharma <sp4250838@gmail.com>
2026-09-20 17:00:45 +0530

    Added UserProfile and improved searchbar

--- github-explorer/src/pages/UserProfile.jsx
+++ github-explorer/src/pages/UserProfile.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const UserProfile = () => {
+  const {username} = useParams();
+
+  const [user, setUser] = useState(null)
+
+  useEffect(()=>{
+    // API REQUEST
+  })
+
   return (
     <div>
+      <h1>User Profile</h1>
+      <p>{username}</p>
       
     </div>
   )
